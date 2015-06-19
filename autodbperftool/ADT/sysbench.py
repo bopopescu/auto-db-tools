449de5c8e05798ffa36aef962711bf0f911fcfc0
mizhon <mizhon@live.cn>
2015-06-19 23:57:42 +0800

    Add Utility, ADT models to project

---
+++ autodbperftool/ADT/sysbench.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+'''
+Created on 2015-06-18
+
+@author: mizhon
+'''
+
---
+++ autodbperftool/ADT/tpccmysql.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+'''
+Created on 2015-06-19
+
+@author: mizhon
+'''
