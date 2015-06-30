c08c98887e884e6a0d6413a5a097ff49a9d31e49
mizhon <mizhon@live.cn>
2015-06-30 22:05:02 +0800

    Add and update tpccmysql functions

--- autodbperftool/ADT/tpccmysql.py
+++ autodbperftool/ADT/tpccmysql.py
@@ -5,3 +5,29 @@ Created on 2015-06-19
 
 @author: mizhon
 '''
+#from common import CommonActions
+from Logs import logger
+log = logger.Log()
+
+class TpccmysqlActions(object):
+    
+    @classmethod
+    def ta_get_cmds(cls, cmd_action):
+        try:
+            cmds = None
+            if cmd_action == 'prepare':
+                pass
+            
+            elif cmd_action == 'run':
+                pass
+            
+            elif cmd_action == 'cleanup':
+                pass
+            
+            return cmds
+        except Exception as e:
+            log.error(e)
+    
+    @classmethod
+    def ta_get_scenario_info(cls):
+        pass
