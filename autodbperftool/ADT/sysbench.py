83414ddcface8a4dceb7e8f56bbe277b390df9e2
mizhon <mizhon@live.cn>
2015-06-30 22:04:31 +0800

    add sysbench functions for execution commands

--- autodbperftool/ADT/sysbench.py
+++ autodbperftool/ADT/sysbench.py
@@ -7,8 +7,30 @@ Created on 2015-06-18
 '''
 
 #from Utility.util import SysbenchUtility
+from Logs import logger
+log = logger.Log()
+
 
 class SysbenchActions(object):
     
-    pass
+    @classmethod
+    def sa_get_cmds(cls, cmd_action):
+        try:
+            cmds = None
+            if cmd_action == 'prepare':
+                pass
+            
+            elif cmd_action == 'run':
+                pass
+            
+            elif cmd_action == 'cleanup':
+                pass
+            
+            return cmds
+        except Exception as e:
+            log.error(e)
+
+    @classmethod
+    def sa_get_scenario_info(cls):
+        pass
 
